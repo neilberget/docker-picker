5c6bb08f3ce762be11f462c1994c91abec4881f9
Neil Berget <neil.berget@gmail.com>
2014-09-16 13:46:49 -0700

    added dtop, dstopall, and dfree

--- docker-picker.sh
+++ docker-picker.sh
@@ -18,6 +18,15 @@ dlogs() {
   eval "history -s $COMMAND"
 }
 
+dtop() {
+  _picker ID
+
+  local COMMAND="docker top $ID"
+
+  eval $COMMAND
+  eval "history -s $COMMAND"
+}
+
 dinspect() {
   _picker ID
 
@@ -44,6 +53,28 @@ ddestroy() {
 
   eval $COMMAND
   eval "history -s $COMMAND"
+}
+
+dstopall() {
+  local COMMAND="docker stop $(docker ps -a -q)"
+  eval $COMMAND
+  eval "history -s $COMMAND"
+}
+
+dfree() {
+  # local COMMAND="docker rm \`docker ps -a | grep Exited | awk '{print \$1 }'\`"
+  # local COMMAND="docker rm \`docker ps -a | awk '{print \$1 }'\`"
+  local COMMAND="docker ps -a | awk '{print \$1}' | xargs docker rm"
+  echo $COMMAND
+  eval $COMMAND
+  eval "history -s $COMMAND"
+
+  local COMMAND="docker rmi \`docker images -aq\`"
+  echo $COMMAND
+  eval $COMMAND
+  eval "history -s $COMMAND"
+
+  # docker images | grep none | awk '{print $3}' | xargs docker rmi
 }
 
 # Helper method used by all of above to invoke vim with docker ps contents
